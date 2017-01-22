6c922e86627de2cf59717d7740a10c70c4cdccc1
DM <dmaharana@gmail.com>
2017-01-22 23:28:21 +0530

    used csv lib to write to csv

--- nn.py
+++ nn.py
@@ -6,7 +6,10 @@ import os.path
 import random
 import time
 import logging
+import csv
+import errno
 
+searchString = 'naked news'
 #soup.find_all('td', class_ = 'sn')
 #soup.find_all('td', class_ = 'sy')
 #soup.find_all('a', style = re.compile('color'))[2].get_text()
@@ -27,7 +30,7 @@ fh = logging.FileHandler(mylogFile)
 fh.setLevel(logging.DEBUG)
 # create console handler with a higher log level
 ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
+ch.setLevel(logging.INFO)
 # create formatter and add it to the handlers
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
@@ -38,56 +41,69 @@ logger.addHandler(ch)
 
 
 def return_url_content(myURL):
-    logger.info('Entering: return_url_content')
+    logger.debug('Entering: return_url_content')
     soup = ''
     logger.info('Parsing URL: {0}'.format(myURL))
 
     waitTime = random.randrange(3,10,1)
     logger.info('Waiting for {0} seconds before hitting the URL'.format(waitTime))
     time.sleep(waitTime)
+    #headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    headers = {'User-Agent': 'Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36'}
 
-    res = requests.get(myURL)
+    res = requests.get(myURL, headers = headers)
     
     try:
         res.raise_for_status()
-        soup = bs4.BeautifulSoup(res.text, 'lxml')
+        #soup = bs4.BeautifulSoup(res.text, 'lxml')
+        soup = bs4.BeautifulSoup(res.text, "html.parser")
     except Exception as exc:
         logger.error('There was a problem: %s' % (exc))
 	soup = ''
 
-    logger.info('Exiting: return_url_content')
+    logger.debug('Exiting: return_url_content')
     return soup
 
 def list_content(soup):
-    logger.info('Entering: list_content')
+    logger.debug('Entering: list_content')
     result = []
     contents = soup.find_all('a', title = re.compile('view Naked News'))
     for content in contents:
 	name = content.get_text()
 	href = content.get('href')
         result.append([name,href])
-    logger.info('Exiting: list_content')
+    logger.debug('Exiting: list_content')
     return result
 
 def list_others(soup):
-    logger.info('Entering: list_others')
+    logger.debug('Entering: list_others')
     result = []
     contents = soup.find_all('a', title = re.compile('View Torrent Info:'))
     for content in contents:
 	name = content.get('title').split(':')[-1].strip()
 	href = content.get('href')
         result.append([name,href])
-    logger.info('Exiting: list_others')
+    logger.debug('Exiting: list_others')
     return result
 
 def return_magnet(soup):
-    logger.info('Entering: return_magnet')
+    logger.debug('Entering: return_magnet')
     mlink = soup.find('a', title = 'Magnet link').get('href')
-    logger.info('Exiting: return_magnet')
+    logger.debug('Exiting: return_magnet')
     return mlink
 
+def print_list2CSV(contentList, outputFile):
+    logger.debug('Entering: print_list2CSV')
+
+    print contentList
+    with open(outputFile, 'ab') as fh:
+         csvWriter = csv.writer(fh, quoting = csv.QUOTE_ALL)
+	 csvWriter.writerows(contentList)
+
+    logger.debug('Exiting: print_list2CSV')
+
 def print_list(contentList, outputFile):
-    logger.info('Entering: print_list')
+    logger.debug('Entering: print_list')
 
     if not os.path.isfile(outputFile):
        fh = open(outputFile, 'w')
@@ -100,18 +116,30 @@ def print_list(contentList, outputFile):
         fh.write(','.join(content)+'\n')
 
     fh.close()
-    logger.info('Exiting: print_list')
+    logger.debug('Exiting: print_list')
+
+def mkdir_path(path):
+    try:
+       os.makedirs(path)
+    except os.error, e:
+       if e.errno != errno.EEXIST:
+          raise
 
 def main():
-    logger.info('Entering: main')
+    logger.debug('Entering: main')
     startTime = time.time()
     maxMagnet = 5
     count = 0
     fileExt = '.csv'
-    outputFile = str(int(time.time()))+fileExt
+    outputDir = 'output'
+    mkdir_path(outputDir)
 
-    searchURL = 'https://extratorrent.cc/search/?search=naked+news&new=1&x=0&y=0'
+    outputFile = str(int(time.time()))+fileExt
+    outputFile = (os.path.join(outputDir, outputFile))
+
+    #searchURL = 'https://extratorrent.cc/search/?search=naked+news&new=1&x=0&y=0'
     baseURL = 'https://extratorrent.cc'
+    searchURL = baseURL+'/search/?search='+searchString+'&new=1&x=0&y=0'
     soup = return_url_content(searchURL)
     if soup:
        contentList = list_content(soup)
@@ -119,6 +147,7 @@ def main():
        for content in contentList:
            partURL = content[1]
            soupm = return_url_content(baseURL+partURL)
+           content[1] = baseURL + partURL
 	   if soupm:
               mLink = return_magnet(soupm)
 	      if mLink:
@@ -127,7 +156,7 @@ def main():
 	      if count > maxMagnet:
 	         break
     
-       print_list(contentList, outputFile)
+       print_list2CSV(contentList, outputFile)
 
        contentList = list_others(soup)
        for content in contentList:
@@ -138,10 +167,10 @@ def main():
 	      if mLink:
 	         content.append(mLink)
 
-       print_list(contentList, outputFile)
+       print_list2CSV(contentList, outputFile)
     else:
        logger.error('No data\n')
 
     logger.info('Time taken: -------- {0} seconds ----------'.format(str(time.time() - startTime)))
-    logger.info('Exiting: main')
+    logger.debug('Exiting: main')
 main()
